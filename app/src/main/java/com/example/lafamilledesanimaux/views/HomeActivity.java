cad980110e9c424cf39f8b405ad85a01153e375a
Tahiana <tahianarakotolobo@gmail.com>
2022-05-13 19:33:22 +0300

    splash screen mnola ho modifierna ilay imageview satria ratsy be

--- app/src/main/java/com/example/lafamilledesanimaux/views/HomeActivity.java
+++ app/src/main/java/com/example/lafamilledesanimaux/views/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.example.lafamilledesanimaux;
+package com.example.lafamilledesanimaux.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.lafamilledesanimaux.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/com/example/lafamilledesanimaux/views/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lafamilledesanimaux.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.lafamilledesanimaux.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int splash_time_out = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, splash_time_out);
+    }
+}
