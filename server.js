a38c933940fbccde35c7c3dbb76036ad48aed23a
Zinah <rzinahrajerison2000@gmail.com>
2022-05-18 08:25:19 +0300

    update

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next){
 });
 
 app.get('/', function (req, res) {
-  return new reponse(200,"HELLOwORLD",null);
+  res.send( new reponse(200,"HELLOwORLD",null));
 });
 app.get('/login',function(req,res){
       new helper().seConnecter().then(function(pool){
