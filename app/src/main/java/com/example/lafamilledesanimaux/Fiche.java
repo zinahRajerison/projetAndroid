38bc570c12d28daf2b9b41ed1e63c83e22b72762
Zinah <rzinahrajerison2000@gmail.com>
2022-05-19 23:43:14 +0300

    Fiche

---
+++ app/src/main/java/com/example/lafamilledesanimaux/Fiche.java
@@ -0,0 +1,14 @@
+package com.example.lafamilledesanimaux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fiche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiche);
+    }
+}
