33fc0ada75dd1a68edf40f20123e1f5fab8e43b6
Zinah <rzinahrajerison2000@gmail.com>
2022-05-20 01:29:49 +0300

    DisplayVideo

--- app/src/main/java/com/example/lafamilledesanimaux/views/Fiche.java
+++ app/src/main/java/com/example/lafamilledesanimaux/views/Fiche.java
@@ -2,7 +2,11 @@ package com.example.lafamilledesanimaux.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.example.lafamilledesanimaux.R;
 
@@ -12,5 +16,20 @@ public class Fiche extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fiche);
+        displayVideo();
     }
+    public void displayVideo(){
+        VideoView view = (VideoView)findViewById(R.id.criAnimal);
+        getResources().getIdentifier("FILENAME_WITHOUT_EXTENSION",
+                "raw", getPackageName());
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.baleine;
+        view.setVideoURI(Uri.parse(path));
+        view.start();
+    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+//        return true;
+//    }
 }
--- app/src/main/java/com/example/lafamilledesanimaux/views/Menu.java
+++ app/src/main/java/com/example/lafamilledesanimaux/views/Menu.java
@@ -49,7 +49,7 @@ public class Menu extends AppCompatActivity {
                         break;
                     case 1:
                         Toast.makeText(Menu.this, "game", Toast.LENGTH_SHORT).show();
-                        Intent intent1=new Intent(Menu.this,List.class);
+                        Intent intent1=new Intent(Menu.this,Fiche.class);
                         startActivity(intent1);
                         break;
                     case 2:
