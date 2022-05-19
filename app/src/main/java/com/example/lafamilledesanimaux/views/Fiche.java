1cf2c7ddc0f1f5b33dd2654dc39cdda41db0754c
Zinah <rzinahrajerison2000@gmail.com>
2022-05-20 02:16:21 +0300

    Video

--- app/src/main/java/com/example/lafamilledesanimaux/views/Fiche.java
+++ app/src/main/java/com/example/lafamilledesanimaux/views/Fiche.java
@@ -2,29 +2,68 @@ package com.example.lafamilledesanimaux.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.example.lafamilledesanimaux.R;
+import com.example.lafamilledesanimaux.models.LoginResponse;
+import com.example.lafamilledesanimaux.models.RetrofitClientInstance;
+import com.example.lafamilledesanimaux.models.User;
+import com.example.lafamilledesanimaux.models.UserResponse;
+import com.example.lafamilledesanimaux.models.UserService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Fiche extends AppCompatActivity {
-
+    VideoView view;
+    private Button btnPlay;
+    private Button btnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fiche);
         displayVideo();
+        init();
+    }
+    private void init() {
+        btnStop = (Button) findViewById(R.id.btnStop);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        submitListener();
+    }
+    /**
+     * ecoute des evenements de login
+     */
+    private void submitListener(){
+        ((Button) findViewById(R.id.btnPlay)).setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+               view.start();
+            }
+        });
+        ((Button) findViewById(R.id.btnStop)).setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                view.stopPlayback();
+            }
+        });
     }
     public void displayVideo(){
-        VideoView view = (VideoView)findViewById(R.id.criAnimal);
+        view = (VideoView)findViewById(R.id.criAnimal);
         getResources().getIdentifier("FILENAME_WITHOUT_EXTENSION",
                 "raw", getPackageName());
         String path = "android.resource://" + getPackageName() + "/" + R.raw.baleine;
         view.setVideoURI(Uri.parse(path));
-        view.start();
     }
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
