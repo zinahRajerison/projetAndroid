02a40d584ca4da44cf5e0dbc01bc6b273328fcc1
Zinah <rzinahrajerison2000@gmail.com>
2022-05-19 21:14:58 +0300

    Update Menu.java

--- app/src/main/java/com/example/lafamilledesanimaux/views/Menu.java
+++ app/src/main/java/com/example/lafamilledesanimaux/views/Menu.java
@@ -2,8 +2,14 @@ package com.example.lafamilledesanimaux.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lafamilledesanimaux.R;
 import com.example.lafamilledesanimaux.models.CardGVAdapter;
@@ -28,5 +34,31 @@ public class Menu extends AppCompatActivity {
 
         CardGVAdapter adapter = new CardGVAdapter(this, courseModelArrayList);
         coursesGV.setAdapter(adapter);
+        submitListener();
+    }
+    private void submitListener() {
+        this.coursesGV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        Toast.makeText(Menu.this, "liste", Toast.LENGTH_SHORT).show();
+                        Intent intent=new Intent(Menu.this,List.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Toast.makeText(Menu.this, "game", Toast.LENGTH_SHORT).show();
+                        Intent intent1=new Intent(Menu.this,List.class);
+                        startActivity(intent1);
+                        break;
+                    case 2:
+                        Toast.makeText(Menu.this, "parametre", Toast.LENGTH_SHORT).show();
+                        Intent intent2=new Intent(Menu.this,List.class);
+                        startActivity(intent2);
+                        break;
+                }
+            }
+        });
     }
 }
