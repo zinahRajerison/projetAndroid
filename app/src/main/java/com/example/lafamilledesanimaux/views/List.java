0f1dd6988af1fc4dacbc2a168f6b796935e3d8ae
Zinah <rzinahrajerison2000@gmail.com>
2022-05-21 00:46:20 +0300

    Fiche

--- app/src/main/java/com/example/lafamilledesanimaux/views/List.java
+++ app/src/main/java/com/example/lafamilledesanimaux/views/List.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
