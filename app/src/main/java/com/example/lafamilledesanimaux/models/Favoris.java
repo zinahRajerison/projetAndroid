a01432c7ca8bed194512f05f52bee5dee5e22b36
Zinah <rzinahrajerison2000@gmail.com>
2022-05-22 16:56:43 +0300

    Update Favoris.java

--- app/src/main/java/com/example/lafamilledesanimaux/models/Favoris.java
+++ app/src/main/java/com/example/lafamilledesanimaux/models/Favoris.java
@@ -1,27 +1,27 @@
 package com.example.lafamilledesanimaux.models;
 
 public class Favoris {
-    private int id_animal;
-    private int id_user;
+    private int idanimal;
+    private int iduser;
 
     public int getId_animal() {
-        return id_animal;
+        return idanimal;
     }
 
     public void setId_animal(int id_animal) {
-        this.id_animal = id_animal;
+        this.idanimal = idanimal;
     }
 
     public int getId_user() {
-        return id_user;
+        return iduser;
     }
 
     public void setId_user(int id_user) {
-        this.id_user = id_user;
+        this.iduser = id_user;
     }
 
     public Favoris(int id_animal, int id_user) {
-        this.id_animal = id_animal;
-        this.id_user = id_user;
+        this.idanimal = id_animal;
+        this.iduser = id_user;
     }
 }
